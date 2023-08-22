cb47fc57e38fbb0510bbe964fb78579039c63a50
Jihyun Lee <zzh23@naver.com>
2023-08-22 17:28:08 +0900

    Feature)LoginView first commit

---
+++ ThreadsTutorials/ThreadsTutorials/App/ThreadsTutorialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsTutorialsApp.swift
+//  ThreadsTutorials
+//
+//  Created by 이지현 on 2023/08/22.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsTutorialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ThreadsTutorials/ThreadsTutorials/Core/Authentication/LoginView.swift
@@ -0,0 +1,82 @@
+//
+//  LoginView.swift
+//  ThreadsTutorials
+//
+//  Created by 이지현 on 2023/08/22.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    @State private var email = ""
+    @State private var password = ""
+    
+    var body: some View {
+        NavigationStack {
+            VStack {
+                Spacer()
+                
+                Image("threads-app-icon")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 128, height: 128)
+                
+                VStack {
+                    TextField("Enter your email", text: $email)
+                    
+                    SecureField("Enter your password", text: $password)
+                }
+                
+                NavigationLink {
+//                    이동한 곳에 뜨는 텍스트
+                    Text("Forgot Password")
+                    
+                } label: {
+                    Text("Forgot Password?")
+                        .font(.footnote)
+                        .fontWeight(.semibold)
+                        .padding(.top)
+                        .padding(.trailing, 28)
+                        .foregroundColor(.black)
+                        .frame(maxWidth: .infinity, alignment: .trailing)
+                }
+                
+                Button {
+                    
+                } label: {
+                    Text("Login")
+                        .font(.subheadline)
+                        .fontWeight(.semibold)
+                        .foregroundColor(.white)
+                        .frame(width: 352, height: 44)
+                        .background(.black)
+                        .cornerRadius(8)
+                }
+                
+                Spacer()
+                
+                Divider()
+                
+                NavigationLink {
+                    Text("Registration View")
+                } label: {
+                    HStack(spacing: 3) {
+                        Text("Don't have an account?")
+                        
+                        Text("Sign Up")
+                            .fontWeight(.semibold)
+                    }
+                    .foregroundColor(.black)
+                    .font(.footnote)
+                }
+                .padding(.vertical, 16)
+            }
+        }
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
